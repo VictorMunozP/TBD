696e8e3815d6fb4d5fedc91daf9c252cbaf6ac88
Javier <javier.pinto.g@usach.cl>
2018-05-14 21:19:04 -0400

    inicio

---
+++ Back_end/TDB_back/src/main/java/com/tdb/back/TdbBackApplication.java
@@ -0,0 +1,12 @@
+package com.tdb.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdbBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdbBackApplication.class, args);
+	}
+}
