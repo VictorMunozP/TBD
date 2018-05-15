f8fa921fdf4cae1429d2a6f9375edf8e8f7edae3
isaAndrea <isamar.hernandez@usach.cl>
2018-05-15 01:39:30 -0400

    Plantilla Front

---
+++ Front_end/plantilla-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <div>
+    <header>
+      <div class="branding">
+        <img class ="logo-small" src="./img/joystick.png" />
+        <span>WorstBest</span>
+      </div>
+      <nav>
+        <a href="#/">Home</a>
+        <a href="#/new">Agregar Empresa</a>
+        <a href="#/new">Gestionar Empresas</a>         
+      </nav>
+    </header>
+
+    <main class="container">
+      <router-view></router-view>
+    </main>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
